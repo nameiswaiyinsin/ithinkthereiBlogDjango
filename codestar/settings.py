c6955f5f3e55923638159c9ab8758bca6a45001f
nameiswaiyinsin <waiyinsin@gmail.com>
2023-08-10 21:03:40 +0000

    Created Post Model and Comment Model diagram into usable Django models in models.py and created Post Admin and Comments Admin in admin.py. SEE: User stories completed: Manage Post, Create Draft posts and Approve Comments. Also added summernote library in django in admin.py, urls.py paths and settings.py INSTALLED APPS

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoblogcodestar.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-nameiswaiyi-ithinkthere-zxjdhqx16hs.ws-eu102.gitpod.io']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
